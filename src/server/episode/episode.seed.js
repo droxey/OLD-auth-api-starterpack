8ac0deff0b8b3ef846199f881048f28444fda92d
kendrajmoore <kendra.j.moore3443@gmail.com>
2018-10-11 07:07:59 -0700

    improve seed route

--- src/server/episode/episode.seed.js
+++ src/server/episode/episode.seed.js
@@ -3,7 +3,7 @@ module.exports = [
   {
     title: 'Farm',
     summary: 'Carrie and Fred visit a farm.',
-    image: 'https://cdn3.bigcommerce.com/s-a6pgxdjc7w/products/1075/images/967/416130__50605.1467418920.1280.1280.jpg?c=2',
+    image: 'https://www.agri-pulse.com/ext/resources/images/Agriculture-photos/Farmscapes/Farm1.jpg?1509734744',
     number: 1,
     season: 1
 
@@ -12,48 +12,48 @@ module.exports = [
   {
     title: 'A Song for Portland',
     summary: 'The mayor wants an anthem for Portland.',
-    image: 'http://www.clipartbest.com/cliparts/9cz/rMM/9czrMMBcE.jpeg',
+    image: 'https://laughingsquid.com/wp-content/uploads/2014/08/portlandia-the-dream-of-the-1890.jpg',
     number: 2,
     season: 1
   },
   {
     title: 'Aimme',
     summary: 'Someone loves Aimee',
-    image: 'http://bluelips.com/prod_images_large/bones1.jpg',
+    image: 'https://img.huffingtonpost.com/asset/5bad5d9b250000cf00377e28.jpeg?ops=scalefit_720_noupscale',
     number: 3,
     season: 1
   },
   {
     title: 'Mayor ',
-    image: 'https://image00.deviantart.net/4cac/i/2013/115/2/1/red_water_rose_by_feferest-d62zxlw.jpg',
+    image: 'https://www.indiewire.com/wp-content/uploads/2016/02/portlandia-kyle-maclachlan.jpg?w=780',
     summary: 'Carrie and Fred must find the mayor',
     number: 4,
     season: 1
   },
   {
     title: 'Blunderbuss',
-    image: 'http://i45.tinypic.com/2e3sbwz.jpg',
+    image: 'https://images.amcnetworks.com/ifc.com/wp-content/uploads/2010/12/PORTLANDIA-105_STILL2-800x450.jpg',
     summary: 'Um ...Portland stuff',
     number: 5,
     season: 1
   },
   {
     title: 'Baseball',
-    image: 'http://otlibrary.com/wp-content/gallery/mantis-shrimp/mantis-shrimp.jpg',
+    image: 'https://images.amcnetworks.com/ifc.com/wp-content/uploads/2010/12/PORTLANDIA-106_STILL2-800x450.jpg',
     summary: 'Everyone loves baseball',
     number: 6,
     season: 1
   },
   {
     title: 'Mixologist',
-    image: 'http://www.thehealthjournals.com/wp-content/uploads/2014/02/kohlrabi-1-810x657.jpg',
+    image: 'https://i.pinimg.com/originals/ae/e2/4c/aee24c3a1a5aa56ced174546cca96124.jpg',
     summary: 'Wine is good',
     number: 1,
     season: 2
   },
   {
     title: 'One Moore Episode',
-    image: 'https://s-media-cache-ak0.pinimage.com/564x/a8/4f/05/a84f051bf47e41382e4becd4a3d05214.jpg',
+    image: 'https://images.amcnetworks.com/ifc.com/wp-content/uploads/2017/11/OneMooreEpisode-MPX.jpg',
     summary: 'Need more Portlandia',
     number: 2,
     season: 2
